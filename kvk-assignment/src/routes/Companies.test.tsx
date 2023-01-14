92165c86965b8841e50544808d52c26735cd62c2
Hee Chan <h.van.der.haar@amsterdam.nl>
2023-01-14 16:28:56 +0100

    tests for Copmanies component

---
+++ kvk-assignment/src/routes/Companies.test.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import * as Hooks from "../hooks/useCompaniesQuery";
+
+import { render, screen } from "@testing-library/react";
+import { Companies } from "./Companies";
+import { UseQueryResult } from "react-query";
+import { CompanyAPIResponse } from "../models/company.model";
+
+it("should render component", () => {
+  jest.spyOn(Hooks, "useCompaniesQuery").mockReturnValue({} as any);
+  render(<Companies />);
+});
+
+it("should render list with items", () => {
+  // TODO: Move to fixtures
+  const mockData = [
+    {
+      id: "1",
+      name: "Wintheiser Group",
+      city: "West Esteban",
+      zipCode: "97018",
+      streetName: "Lilly View",
+      logo: "https://via.placeholder.com/150",
+      createdAt: "2021-07-16T19:41:28.272Z",
+    },
+    {
+      id: "2",
+      name: "Feest, Schinner and Lowe",
+      city: "New Ahmad",
+      zipCode: "07811",
+      streetName: "Bartell Tunnel",
+      logo: "https://via.placeholder.com/150",
+      createdAt: "2021-10-03T18:37:01.931Z",
+    },
+  ] as any;
+
+  jest
+    .spyOn(Hooks, "useCompaniesQuery")
+    .mockReturnValue({ data: { total: 2, data: mockData } } as UseQueryResult<
+      CompanyAPIResponse,
+      unknown
+    >);
+
+  render(<Companies />);
+
+  //  Maybe not the best selector but stumble upon some limitation of setting test-id on MUI components
+  const listItem1 = screen.getByText("Wintheiser Group");
+  const listItem2 = screen.getByText("Feest, Schinner and Lowe");
+
+  expect(listItem1).toBeVisible();
+  expect(listItem2).toBeVisible();
+});
--- kvk-assignment/src/routes/Companies.tsx
+++ kvk-assignment/src/routes/Companies.tsx
@@ -22,7 +22,7 @@ export function Companies() {
       <div>
         <div>Search From with Button</div>
 
-        <div className={styles.listContainer} data-testid="">
+        <div className={styles.listContainer} data-testid="companies-list">
           <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
               <TableHead>
