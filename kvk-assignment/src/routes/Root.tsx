1033b271f9b980ab1af4d8f68349b52534f273d7
Hee Chan <h.van.der.haar@amsterdam.nl>
2023-01-15 14:03:14 +0100

    redirect to companies from home

--- kvk-assignment/src/routes/Root.tsx
+++ kvk-assignment/src/routes/Root.tsx
@@ -1,7 +1,16 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, useParams } from "react-router-dom";
 
 export function Root() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (!id) {
+      navigate("/companies");
+    }
+  }, [id, navigate]);
+
   return (
     <main>
       <div>
